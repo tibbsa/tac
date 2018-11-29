78b600f6da29a4bde8b03ebe1ee6ae7dca4605bf
Anthony Tibbs <anthony@tibbs.ca>
2018-11-28 22:08:47 -0500

    Nov 28 adjustments to fix orientation of Landolt C characters and make spacing adjustments

--- make_landolt.php
+++ make_landolt.php
@@ -24,8 +24,8 @@
 // All measurements in inches
 $page_width_in = 8.5;
 $page_height_in = 11;
-$interline_spacing_in = 0.55;
-$top_margin_in = 0.2;
+$interline_spacing_in = 0.6;
+$top_margin_in = .65;
 $left_margin_in = 0.2;
 
 // Manual measurement of the resulting production is required to confirm
@@ -69,7 +69,7 @@ $page_dpi = 300;
 $page_width_px = $page_dpi * $page_width_in;
 $page_height_px = $page_dpi * $page_height_in;
 $interline_spacing_px = $page_dpi * $interline_spacing_in;
-$character_cell_spacing = $page_width_px / 8 - 25;
+$character_cell_spacing = $page_width_px / 8 - 10;
 $left_margin_px = $left_margin_in * $page_dpi;
 $top_margin_px = $top_margin_in * $page_dpi;
 
@@ -94,7 +94,7 @@ foreach ($chart_line_specs as $clLabel => $clSpecs) {
         $angle = 0;
         switch ($clSpecs['sequence'][$i]) {
         case 'u' :
-            $angle = 270; break;
+            $angle = 90; break;
             
         case 'l' :
             $angle = 180; break;
@@ -103,7 +103,7 @@ foreach ($chart_line_specs as $clLabel => $clSpecs) {
             $angle = 0; break;
             
         case 'd':
-            $angle = 90; break;
+            $angle = 270; break;
 
         default:
             die ('Error in sequence for ' . $clLabel . ': unrecognized direction "' . $clSpecs['sequence'][$i] . '" (pos ' . $i . ')');
